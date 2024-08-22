811555dcc72201aa823aae0554dc55202f55132a
dainsiby <dainsiby14@gmail.com>
2024-08-22 10:31:18 +0530

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -1,59 +1,53 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+void main() 
 {
-int i=0,j,n,small,c=0,a[100],temp;
-c=c+2;
-printf("Enter the no. of elements");
-c++;
-scanf("%d",&n);
-c++;
-printf("Enter the elements");
-c++;
-c++;
-for(i=0;i<n;i++)
-{
-c++;
-scanf("%d",&a[i]);
-c++;
-}
-c++;
-for(i=0;i<n-1;i++)
-{
-c++;
-small=i;
-c++;
-c++;
-for(j=i+1;j<n;j++)
-{
-c++;
-c++;
-if(a[j]<a[small])
-{
-c++;
-small=j;
-}
-}
-c++;
-if(small!=i)
-{
-temp=a[i];
-c++;
-a[i]=a[small];
-c++;
-a[small]=temp;
-c++;
-}
-}
-printf("sorted List is:\n");
-c++;
-c++;
-for(i=0;i<n;i++)
-{
-c++;
-printf("%d \t",a[i]);
-c++;
-}
-printf("\n");
+    int n,i,j,temp,c=0,a[100];
+    c=c+1;
+    printf("Enter the number of elements: ");
+    c++;
+    scanf("%d", &n);
+    c++;
+    printf("Enter the elements:\n");
+    c++;
+    c++;
+    for(i=0;i<n;i++) 
+    {
+  	c++;  
+        scanf("%d", &a[i]);
+    	c++;
+    }
+    c++;
+    for(i=1;i<n;i++) 
+    {
+ 	c++;
+        temp=a[i];
+        c++;
+        j=i-1;
+        c++;
+    
+     c++;
+     while(j>=0 && a[j]>temp) 
+      {
+      		c++;
+          a[j+1]=a[j];
+          c++;
+    	  j--;
+      }
+   
+      a[j+1]=temp;
+      c++;
+    }
+    printf("Sorted array:\n");
+    c++;
+    c++;
+    for(i=0;i<n;i++) 
+    {
+    c++;
+    c++;
+    printf("%d\t",a[i]);
+    }
+    printf("\n");
+    c++;
 printf("\nThe space complexity is %d",(5*4)+(n*4));
  c++;
  printf("\nThe time complexity is %d",c++);
